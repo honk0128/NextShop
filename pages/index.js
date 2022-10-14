183e745c4049614d7d639b6c8f1bedd4cc9ea929
honk0128 <honk0128@gmail.com>
2022-10-14 18:51:05 +0900

    commit13

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import { React } from 'react';
 export default function Home() {
   return (
     <Layout title="Home">
-      <div className="grid grid-cols1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      <div className="grid grid-cols1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {data.products.map((product) => (
           <ProductItem product={product} key={product.slug} />
         ))}
