56f70780d2eb395017206c21724a53126d2034e4
honk0128 <honk0128@gmail.com>
2022-10-14 18:40:55 +0900

    commit13

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import data from '../utils/data';
 import Layout from '../components/Layout';
 import ProductItem from '../components/Productitems';
-import styles from '../styles/Home.module.css';
-import data from '../utils/data';
+import { React } from 'react';
 
 export default function Home() {
   return (
-    <Layout title="Home Page">
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+    <Layout title="Home">
+      <div className="grid grid-cols1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
         {data.products.map((product) => (
-          <ProductItem product={product} key={product.slug}></ProductItem>
+          <ProductItem product={product} key={product.slug} />
         ))}
       </div>
     </Layout>
