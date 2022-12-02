1261098282297f82d912c2343d8f6f2bf823a25c
honk0128 <honk0128@gmail.com>
2022-12-02 10:46:58 +0900

    14. loginlog

--- pages/login.js
+++ pages/login.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import { signIn, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import Layout from '../components/Layout';
+import { signIn, useSession } from 'next-auth/react';
 import { useForm } from 'react-hook-form';
+import Layout from '../components/Layout';
 import { getError } from '../utils/error';
 import { toast } from 'react-toastify';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
 export default function LoginScreen() {
   const { data: session } = useSession();
   const router = useRouter();
   const { redirect } = router.query;
+
   useEffect(() => {
     if (session?.user) {
       router.push(redirect || '/');
@@ -31,6 +32,13 @@ export default function LoginScreen() {
         email,
         password,
       });
+
+      await axios
+        .post('/api/auth/loginLog', { provider: 'credentials' })
+        .then((res) => {
+          console.log(res.data.message);
+        });
+
       if (result.error) {
         toast.error(result.error);
       }
@@ -41,10 +49,16 @@ export default function LoginScreen() {
 
   const githubLoginHandler = async () => {
     try {
+      // eslint-disable-next-line no-unused-vars
       const result = await signIn('github', {
         redirect: false,
       });
-      console.log('Github login: ' + result);
+
+      await axios
+        .post('/api/auth/loginLog', { provider: 'github' })
+        .then((res) => {
+          console.log(res.data.message);
+        });
     } catch (err) {
       toast.error(getError(err));
     }
@@ -56,6 +70,12 @@ export default function LoginScreen() {
       const result = await signIn('google', {
         redirect: false,
       });
+
+      await axios
+        .post('/api/auth/loginLog', { provider: 'google' })
+        .then((res) => {
+          console.log(res.data.message);
+        });
     } catch (err) {
       toast.error(getError(err));
     }
@@ -67,6 +87,12 @@ export default function LoginScreen() {
       const result = await signIn('kakao', {
         redirect: false,
       });
+
+      await axios
+        .post('/api/auth/loginLog', { provider: 'kakao' })
+        .then((res) => {
+          console.log(res.data.message);
+        });
     } catch (err) {
       toast.error(getError(err));
     }
@@ -78,6 +104,12 @@ export default function LoginScreen() {
       const result = await signIn('naver', {
         redirect: false,
       });
+
+      await axios
+        .post('/api/auth/loginLog', { provider: 'naver' })
+        .then((res) => {
+          console.log(res.data.message);
+        });
     } catch (err) {
       toast.error(getError(err));
     }
@@ -89,54 +121,56 @@ export default function LoginScreen() {
         className="mx-auto max-w-screen-md"
         onSubmit={handleSubmit(submitHandler)}
       >
-        <h1 className="mb-4 text-xl">Login</h1>
-        <div className="mb-4">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            {...register('email', {
-              required: 'Please enter email',
-              pattern: {
-                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-                message: 'Please enter valid email',
-              },
-            })}
-            className="w-full"
-            id="email"
-            autoFocus
-          />
-          {errors.email && (
-            <div className="text-red-500">{errors.email.message}</div>
-          )}
+        <h1 className="mb-4 text-3xl">Login </h1>
+        <div className="p-5 bg-blue-200 rounded-lg mb-4">
+          <div className="mb-4">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              {...register('email', {
+                required: 'Please enter email',
+                pattern: {
+                  value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                  message: 'Please enter valid email',
+                },
+              })}
+              className="w-full"
+              id="email"
+              autoFocus
+            />
+            {errors.email && (
+              <div className="text-red-500">{errors.email.message}</div>
+            )}
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              {...register('password', {
+                required: 'Please enter password',
+                minLength: {
+                  value: 3,
+                  message: 'password is more than 3 chars',
+                },
+              })}
+              className="w-full"
+              id="password"
+              autoFocus
+            />
+            {errors.password && (
+              <div className="text-red-500 ">{errors.password.message}</div>
+            )}
+          </div>
+          <div className="mb-4">
+            <button className="primary-button">Login</button>
+          </div>
+          <div className="mb-4">
+            Don&apos;t have an account? &nbsp;
+            <Link href={`/register?redirect=${redirect || '/'}`}>Register</Link>
+          </div>
         </div>
 
-        <div className="mb-4">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            {...register('password', {
-              required: 'Please enter password',
-              minLength: { value: 3, message: 'password is more than 3 chars' },
-            })}
-            className="w-full"
-            id="password"
-            autoFocus
-          />
-          {errors.password && (
-            <div className="text-red-500 ">{errors.password.message}</div>
-          )}
-        </div>
-
-        <div className="mb-4">
-          <button className="primary-button">Login</button>
-        </div>
-
-        <div className="mb-4">
-          Don&apos;t have an account? &nbsp;
-          <Link href="register">Register</Link>
-        </div>
-
-        <div className="p-5 bg-slate-500 rounded-lg">
+        <div className="p-5 bg-gray-400 rounded-lg">
           <div className="mb-4">
             <button
               className="primary-button w-full"
@@ -167,7 +201,7 @@ export default function LoginScreen() {
             </button>
           </div>
 
-          <div className="">
+          <div>
             <button
               className="primary-button w-full"
               type="button"
